2573a7c0fd1b6344242b32a258a5f35e6e20efcb
QuinnMarshal <marshallq4711@gmail.com>
2023-07-19 18:28:45 -0700

    added nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import { Link } from 'react-router-dom'
 import React, {useState} from 'react'
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
   return (
     <>
       <nav className='navbar'>
@@ -37,6 +50,7 @@ function Navbar() {
                     </Link>
                 </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>Hi</Button>}
         </div>
       </nav>
     </>
