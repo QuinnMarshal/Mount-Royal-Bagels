2f2bdef7bb1ea28d310d816e3b506d0b45507a84
QuinnMarshal <marshallq4711@gmail.com>
2023-11-14 16:24:47 -0800

    edited contact us page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,17 +9,6 @@ function Contact() {
                 <div class="contact-container">
 
                     <div class="map-section">
-                        <h2>Visit Us</h2>
-                        <br></br>
-                        <iframe title="my map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10283.884271454173!2d-119.4643513!3d49.8805699!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537d8cad83cc7b75%3A0x5992cb1462cc0836!2sMount%20Royal%20Bagel%20Factory!5e0!3m2!1sen!2sca!4v1699499001903!5m2!1sen!2sca"
-                            width="900"
-                            height="450"
-                            style={{ border: 0 }}
-                            allowfullscreen=""
-                            loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade">
-                        </iframe>
-                    </div>
 
                     <div class="contact-info">
                         <div class="contact-item">
@@ -40,6 +29,17 @@ function Contact() {
                                 <i class="fab fa-facebook"></i>
                             </a>
                         </div>
+                    </div>
+                    <br></br>
+
+                        <iframe title="my map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10283.884271454173!2d-119.4643513!3d49.8805699!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537d8cad83cc7b75%3A0x5992cb1462cc0836!2sMount%20Royal%20Bagel%20Factory!5e0!3m2!1sen!2sca!4v1699499001903!5m2!1sen!2sca"
+                            width="900"
+                            height="450"
+                            style={{ border: 0 }}
+                            allowfullscreen=""
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
                     </div>
                 </div>
             </body>
