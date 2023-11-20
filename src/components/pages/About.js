3750fa35340096e06965bf3abef4f6a4c8d15a9d
QuinnMarshal <marshallq4711@gmail.com>
2023-11-20 15:53:18 -0800

    edited about us page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,16 +13,15 @@ function About() {
             </div>
             <div class="text-box">
                 <h2>Our Story</h2>
-                <p style={{marginBottom: 20}}>Welcome to the Mount Royal Bagel Factory, your destination for the finest Montreal style bagels in the heart of Kelowna, BC. Our journey started with a passion for the authentic taste of Montreal bagels, and a commitment to bringing that tradition to the beautiful Okanagan Valley.</p>
+                <p style={{marginBottom: 20, wordSpacing: "1px", lineHeight: "1.4"}}>Welcome to the Mount Royal Bagel Factory, where the enticing aroma of freshly baked Montreal-style bagels welcomes you to Kelowna, BC. Our story began with a love for the traditional taste of Montreal bagels, driving our commitment to bring that iconic flavor to the stunning Okanagan Valley.</p>
 
                 <h2>What Makes Us Special</h2>
-                <p style={{marginBottom: 20}}>At Mount Royal Bagel Factory, we take pride in making our bagels fresh daily, right here in our store. Our team of skilled bakers follows the time-honored Montreal tradition, hand-rolling each bagel to perfection. The result is a bagel that's crispy on the outside, soft and chewy on the inside, and bursting with flavor. Don't forget about our wide variety of delicious coffees that pair perfectly with one of our mouthwatering bagel sandwiches.</p>
+                <p style={{marginBottom: 20, wordSpacing: "1px", lineHeight: "1.4"}}>At Mount Royal Bagel Factory, excellence is baked into every bagel. Each day, our skilled bakers meticulously follow the time-honored Montreal tradition, hand-rolling and crafting every bagel to perfection. It's this artisanal touch that gives our bagels their distinct qualities – a crispy exterior, a soft and chewy interior, and a symphony of flavors. Pair our exceptional bagel sandwiches with a wide selection of gourmet coffees, creating a delightful harmony of taste and aroma.</p>
 
                 <h2>Visit Us</h2>
-                <p style={{marginBottom: 20}}>We invite you to visit our store, where the aroma of freshly baked bagels will greet you at the door. Whether you're a long-time bagel enthusiast or trying Montreal style bagels for the first time, we promise an unforgettable experience. Check out our menu!</p>
-
+                <p style={{marginBottom: 20, wordSpacing: "1px", lineHeight: "1.4"}}>Step into our store and experience more than just delicious bagels; it's an immersion into a tradition that's been cherished for generations. The moment you walk through our doors, the warm, fragrant embrace of our freshly baked bagels will captivate your senses. Whether you're a seasoned bagel connoisseur or venturing into the world of Montreal-style bagels for the first time, our promise remains the same – an unforgettable culinary journey. Explore our menu to discover a diverse array of tantalizing options that will satisfy your cravings.</p>
                 <h2>Contact Us</h2>
-                <p style={{marginBottom:20}}>If you have any questions or feedback, don't hesitate to get in touch. We're here to serve you with the best Montreal style bagels in Kelowna.</p>
+                <p style={{marginBottom:20, wordSpacing: "1px", lineHeight: "1.4"}}>Your satisfaction is our priority. Should you have any inquiries, feedback, or simply want to share your bagel experience with us, please don't hesitate to reach out. We're dedicated to serving you the finest Montreal-style bagels in Kelowna and are eager to ensure your experience with us exceeds expectations.</p>
                 <a href="/contact" class="contact-button">Contact Us</a>
             </div>
             <div class="clear"></div>
