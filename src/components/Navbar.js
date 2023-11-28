93781f3b67c5765f4ed50665a8352521fc8e9665
QuinnMarshal <marshallq4711@gmail.com>
2023-11-28 11:17:05 -0800

    removed button from navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,11 @@
 import { Link } from 'react-router-dom';
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false)
-        } else {
-            setButton(true)
-        }
-    };
-
-    useEffect(() => {
-        showButton()
-    }, []);
-
-    window.addEventListener('resize', showButton);
 
   return (
     <>
